7077c412d7855d32f2b3189a8b2f8f3e239d9225
vishalkondle.dev <vishalkondle.dev@gmail.com>
2024-07-18 18:23:49 +0530

    2--html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/index.js',
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
