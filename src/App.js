dcc18db560c4575417153cf6f47fdaf295c3cba7
vishalkondle.dev <vishalkondle.dev@gmail.com>
2024-07-18 19:13:16 +0530

    5--setup-css-and-style-loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './App.css'
+import btnStyles from './Button.module.css'
 
 const App = () => {
     return (
         <div>
             <h1>Hello World 2</h1>
+            <button className={btnStyles.success}>Success</button>
+            <button className={btnStyles.error}>Error</button>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,22 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-react', '@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"], // order matters - Right to Left 
+                // use: [
+                //     { loader: 'style-loader' },
+                //     { loader: 'css-loader', options: { modules: true } } // In case module.css files not working
+                // ]
             }
         ]
     },
